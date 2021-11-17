4a7c49ad4e315123a53b09e6208952565633a553
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-17 00:05:08 -0300

    Create View vw_Career

--- 010-Create-View-vw_Career.sql
+++ 010-Create-View-vw_Career.sql
@@ -1,7 +1,14 @@
+CREATE OR ALTER VIEW vw_Career
+as
 SELECT
     [Career].[Id],
     [Career].[Title],
     [Career].[Url],
-    (SELECT COUNT(CareerId) from [CareerItem] where [CareerId] = [Career].[Id]) as [Courses]
+    COUNT([Id]) AS [Courses]
 FROM
     [Career]
+    INNER JOIN [CareerItem] ON [CareerItem].[CareerId] = [Career].[Id]
+GROUP BY
+    [Career].[Id],
+    [Career].[Title],
+    [Career].[Url]
